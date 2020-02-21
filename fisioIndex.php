3a9619bdc94410955de00db679b7bd0a73d84c86
Mateus Schwede <mateus1908.schwede@gmail.com>
2020-02-20 23:40:44 -0300

    Alteração index fisioterapeuta

--- fisioIndex.php
+++ fisioIndex.php
@@ -1,3 +1,62 @@
-<?php
-    echo $_SESSION['nome']." logado";
-?>
+<?php require_once 'extras/start.php' ?>
+INDEX DO FISIOTERAPEUTA
+<br>
+- Ver sessões fisioterapia(filtro por dia)<br>
+- Cadastrar sessão fisioterapia<br>
+- Editar sessão fisioterapia<br>
+- Realizar sessão fisioterapia<br>
+- Cancelar sessão fisioterapia<br>
+
+
+
+<div class="accordion" id="accordionExample">
+    
+    <!-- Cadastrar Sessão -->
+    <div class="card">
+        <div class="card-header" id="headingOne">
+            <h2 class="mb-0"><button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapse1" aria-expanded="true" aria-controls="collapse1">Cadastrar sessão</button></h2>
+        </div>
+
+        <div id="collapse1" class="collapse" aria-labelledby="headingOne" data-parent="#accordionExample">
+            <div class="card-body">
+                CONTEUDO AQUI
+            </div>
+        </div>
+    </div>
+    
+    <!-- Editar Sessão -->
+    <div class="card">
+        <div class="card-header" id="headingTwo">
+            <h2 class="mb-0"><button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapse2" aria-expanded="false" aria-controls="collapse2">Editar sessão</button></h2>
+        </div>
+        <div id="collapse2" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
+            <div class="card-body">
+                CONTEUDO AQUI
+            </div>
+        </div>
+    </div>
+
+    <!-- Realizar Sessão -->
+    <div class="card">
+        <div class="card-header" id="headingTwo">
+            <h2 class="mb-0"><button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapse3" aria-expanded="false" aria-controls="collapse3">Realizar sessão</button></h2>
+        </div>
+        <div id="collapse3" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
+            <div class="card-body">
+                CONTEUDO AQUI
+            </div>
+        </div>
+    </div>
+
+    <!-- Cancelar Sessão -->
+    <div class="card">
+        <div class="card-header" id="headingTwo">
+            <h2 class="mb-0"><button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapse4" aria-expanded="false" aria-controls="collapse4">Cancelar sessão</button></h2>
+        </div>
+        <div id="collapse4" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
+            <div class="card-body">
+                CONTEUDO AQUI
+            </div>
+        </div>
+    </div>
+</div>
